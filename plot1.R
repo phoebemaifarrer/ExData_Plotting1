a5f06796e9d17a20e02ddd3cf7f9c33906ce659a
Phoebe Farrer <Phoebe.Farrer@CreditBenchmark.local>
2020-04-18 12:20:33 +0100

    Reading and formatting the data set before starting plot 1.

--- plot1.R
+++ plot1.R
@@ -2,4 +2,22 @@
 # By Phoebe Farrer
 # 18-04-2020
 
-# Start
+# Set working directory to read in data set (this will need to be changed for different computers/users) ----
+
+setwd("Z:/Credit Benchmark/Data/Phoebe/Archive/Exploratory Data Analysis")
+
+# Load in data ----
+
+data <- read.table("household_power_consumption.txt", header = T, sep = ";", comment.char = "%", stringsAsFactors = F, na.strings = "?")
+dim(data) # checking I have loaded in the full data set
+head(data) # checking the format of the columns in the data set
+
+# Subset the data set to only be the interest dats i.e. 2007-02-01 and 2007-02-02 ----
+
+data_subset <- subset(data, Date == "1/2/2007" | Date == "2/2/2007")
+
+# Format the data and time columns (for plot 2, 3, and 4) ----
+
+data_subset$DateTimeStamp <- strptime(paste(data_subset$Date, data_subset$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+
+# Plot 1 ----
